21294a45154633aaf0417330af4f6ef29fc719df
Andrew Kyle Knittle <andrk11@hickory.rlogin>
2015-10-21 13:05:42 -0400

    simple test driver that goes through all the basic tests. Improved output.

--- test_runner.sh
+++ test_runner.sh
@@ -1,98 +1,107 @@
 #!/bin/bash
 # runs a bunch of small tests with numerous testing scenarious, for the thread pool.
+echo Basic test...
+echo 5 threads
 ./threadpool_test -n 5
 ./threadpool_test -n 5
 ./threadpool_test -n 5
 ./threadpool_test -n 5
 ./threadpool_test -n 5
 
-
+echo 4 threads
 ./threadpool_test -n 4
 ./threadpool_test -n 4
 ./threadpool_test -n 4
 ./threadpool_test -n 4
 ./threadpool_test -n 4
 
-
+echo 3 threads
 ./threadpool_test -n 3
 ./threadpool_test -n 3
 ./threadpool_test -n 3
 ./threadpool_test -n 3
 ./threadpool_test -n 3
 
-
+echo 2 threads
 ./threadpool_test -n 2
 ./threadpool_test -n 2
 ./threadpool_test -n 2
 ./threadpool_test -n 2
 ./threadpool_test -n 2
 
+echo 1 thread
 ./threadpool_test
 ./threadpool_test
 ./threadpool_test
 ./threadpool_test
 ./threadpool_test
 
+echo Basic test 2...
+echo 5 threads
 ./threadpool_test2 -n 5
 ./threadpool_test2 -n 5
 ./threadpool_test2 -n 5
 ./threadpool_test2 -n 5
 ./threadpool_test2 -n 5
 
-
+echo 4 threads
 ./threadpool_test2 -n 4
 ./threadpool_test2 -n 4
 ./threadpool_test2 -n 4
 ./threadpool_test2 -n 4
 ./threadpool_test2 -n 4
 
-
+echo 3 threads
 ./threadpool_test2 -n 3
 ./threadpool_test2 -n 3
 ./threadpool_test2 -n 3
 ./threadpool_test2 -n 3
 ./threadpool_test2 -n 3
 
-
+echo 2 threads
 ./threadpool_test2 -n 2
 ./threadpool_test2 -n 2
 ./threadpool_test2 -n 2
 ./threadpool_test2 -n 2
 ./threadpool_test2 -n 2
 
+echo 1 thread
 ./threadpool_test2 
 ./threadpool_test2 
 ./threadpool_test2 
 ./threadpool_test2 
 ./threadpool_test2
 
+echo Basic test 3...
+echo 5 threads
 ./threadpool_test3 -n 5
 ./threadpool_test3 -n 5
 ./threadpool_test3 -n 5
 ./threadpool_test3 -n 5
 ./threadpool_test3 -n 5
 
-
+echo 4 threads
 ./threadpool_test3 -n 4
 ./threadpool_test3 -n 4
 ./threadpool_test3 -n 4
 ./threadpool_test3 -n 4
 ./threadpool_test3 -n 4
 
-
+echo 3 threads
 ./threadpool_test3 -n 3
 ./threadpool_test3 -n 3
 ./threadpool_test3 -n 3
 ./threadpool_test3 -n 3
 ./threadpool_test3 -n 3
 
-
+echo 2 threads
 ./threadpool_test3 -n 2
 ./threadpool_test3 -n 2
 ./threadpool_test3 -n 2
 ./threadpool_test3 -n 2
 ./threadpool_test3 -n 2
 
+echo 1 thread
 ./threadpool_test3 
 ./threadpool_test3 
 ./threadpool_test3 
