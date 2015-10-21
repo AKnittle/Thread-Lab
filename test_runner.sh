d51872a492871e3bfa608c85aa1e28fd566a1b1d
Andrew Kyle Knittle <andrk11@hickory.rlogin>
2015-10-21 12:55:52 -0400

    simple test driver that goes through all the basic tests.

---
+++ test_runner.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+# runs a bunch of small tests with numerous testing scenarious, for the thread pool.
+./threadpool_test -n 5
+./threadpool_test -n 5
+./threadpool_test -n 5
+./threadpool_test -n 5
+./threadpool_test -n 5
+
+
+./threadpool_test -n 4
+./threadpool_test -n 4
+./threadpool_test -n 4
+./threadpool_test -n 4
+./threadpool_test -n 4
+
+
+./threadpool_test -n 3
+./threadpool_test -n 3
+./threadpool_test -n 3
+./threadpool_test -n 3
+./threadpool_test -n 3
+
+
+./threadpool_test -n 2
+./threadpool_test -n 2
+./threadpool_test -n 2
+./threadpool_test -n 2
+./threadpool_test -n 2
+
+./threadpool_test
+./threadpool_test
+./threadpool_test
+./threadpool_test
+./threadpool_test
+
+./threadpool_test2 -n 5
+./threadpool_test2 -n 5
+./threadpool_test2 -n 5
+./threadpool_test2 -n 5
+./threadpool_test2 -n 5
+
+
+./threadpool_test2 -n 4
+./threadpool_test2 -n 4
+./threadpool_test2 -n 4
+./threadpool_test2 -n 4
+./threadpool_test2 -n 4
+
+
+./threadpool_test2 -n 3
+./threadpool_test2 -n 3
+./threadpool_test2 -n 3
+./threadpool_test2 -n 3
+./threadpool_test2 -n 3
+
+
+./threadpool_test2 -n 2
+./threadpool_test2 -n 2
+./threadpool_test2 -n 2
+./threadpool_test2 -n 2
+./threadpool_test2 -n 2
+
+./threadpool_test2 
+./threadpool_test2 
+./threadpool_test2 
+./threadpool_test2 
+./threadpool_test2
+
+./threadpool_test3 -n 5
+./threadpool_test3 -n 5
+./threadpool_test3 -n 5
+./threadpool_test3 -n 5
+./threadpool_test3 -n 5
+
+
+./threadpool_test3 -n 4
+./threadpool_test3 -n 4
+./threadpool_test3 -n 4
+./threadpool_test3 -n 4
+./threadpool_test3 -n 4
+
+
+./threadpool_test3 -n 3
+./threadpool_test3 -n 3
+./threadpool_test3 -n 3
+./threadpool_test3 -n 3
+./threadpool_test3 -n 3
+
+
+./threadpool_test3 -n 2
+./threadpool_test3 -n 2
+./threadpool_test3 -n 2
+./threadpool_test3 -n 2
+./threadpool_test3 -n 2
+
+./threadpool_test3 
+./threadpool_test3 
+./threadpool_test3 
+./threadpool_test3 
+./threadpool_test3 
+
+ 
+
+
+
